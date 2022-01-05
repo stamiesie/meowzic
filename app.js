fadf58f0401e4b559e038ef6dd83a7110b8f2d24
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2022-01-05 14:01:37 -0800

    refactor keycode to key, add query selector for key audio

---
+++ app.js
@@ -0,0 +1,8 @@
+window.addEventListener('keydown', function(e){
+    const audio = document.querySelector(`audio[data-key="${e.key}"]`)
+    const key = document.querySelector(`.key[data-key="${e.key}"]`)
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    console.log(key);
+})
