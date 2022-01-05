b80b9060bb5f7779f01e54fc19cef6713b4c2647
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2022-01-05 14:46:22 -0800

    remove transition after new key has been pressed, refactor audio into soundEffects function

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-window.addEventListener('keydown', function(e){
+function soundEffects(e) {
     const audio = document.querySelector(`audio[data-key="${e.key}"]`)
     const key = document.querySelector(`.key[data-key="${e.key}"]`)
     if(!audio) return;
     audio.currentTime = 0;
     audio.play();
-    console.log(key);
-})
+    key.classList.add('playing');
+}
+
+function removeTransition(e) {
+    if(e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', soundEffects)
